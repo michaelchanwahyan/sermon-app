8b9c66441e49c2ca03afe1a22a99dd8b2d8dd51c
michael <michaelchan_wahyan@yahoo.com.hk>
2023-07-08 00:02:11 +0800

    [revamp] add folder structure to build against project folder

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-xelatex sermon2012-18.tex ; xelatex sermon2012-18.tex ; mv sermon2012-18.pdf ../
-xelatex sermon2019-20.tex ; xelatex sermon2019-20.tex ; mv sermon2019-20.pdf ../
-xelatex sermon2021-22.tex ; xelatex sermon2021-22.tex ; mv sermon2021-22.pdf ../
+echo build sermon project $1
+xelatex $1/sermon_$1_2012-18.tex ; xelatex $1/sermon_$1_2012-18.tex ; mv sermon_$1_2012-18.mtc* $1/ ; mv sermon_$1_2012-18.pdf ../
+xelatex $1/sermon_$1_2019-20.tex ; xelatex $1/sermon_$1_2019-20.tex ; mv sermon_$1_2019-20.mtc* $1/ ; mv sermon_$1_2019-20.pdf ../
+xelatex $1/sermon_$1_2021-22.tex ; xelatex $1/sermon_$1_2021-22.tex ; mv sermon_$1_2021-22.mtc* $1/ ; mv sermon_$1_2021-22.pdf ../
