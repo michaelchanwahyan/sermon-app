36d2000b94a4fad9d4b0943e9d47a0c6fd4996ff
michael <michaelchan_wahyan@yahoo.com.hk>
2023-12-12 01:25:22 +0800

    [update] build script ordering; add CBI and WWBS

--- build/build.sh
+++ build/build.sh
@@ -2,10 +2,16 @@
 echo build sermon project $1
 if   [ "$1" == "genall" ] ; then
     cd ../whisper
+    python3 convert_srt2txt.py CBI
     python3 convert_srt2txt.py JNG
+    python3 convert_srt2txt.py WWBS
     python3 convert_srt2txt.py YFCX
     cd ../projects
-    cd ./JNG
+    cd ./CBI
+    python3 ../func_ipynb_2_py.py generate_sermonbook.ipynb
+    python3 generate_sermonbook.py
+    cd ..
+    cd ./DSCCC
     python3 ../func_ipynb_2_py.py generate_sermonbook.ipynb
     python3 generate_sermonbook.py
     cd ..
@@ -13,7 +19,11 @@ if   [ "$1" == "genall" ] ; then
     python3 ../func_ipynb_2_py.py generate_sermonbook.ipynb
     python3 generate_sermonbook.py
     cd ..
-    cd ./DSCCC
+    cd ./JNG
+    python3 ../func_ipynb_2_py.py generate_sermonbook.ipynb
+    python3 generate_sermonbook.py
+    cd ..
+    cd ./WWBS
     python3 ../func_ipynb_2_py.py generate_sermonbook.ipynb
     python3 generate_sermonbook.py
     cd ..
@@ -23,10 +33,39 @@ if   [ "$1" == "genall" ] ; then
     cd ..
     cd ../build
 elif [ "$1" == "buildall" ] ; then
-    ./build.sh JNG
-    ./build.sh HKBC
+    ./build.sh CBI
     ./build.sh DSCCC
+    ./build.sh HKBC
+    ./build.sh JNG
+    ./build.sh WWBS
     ./build.sh YFCX
+elif [ "$1" == "CBI" ] ; then
+    cd $1
+    xelatex sermon_$1.tex
+    xelatex sermon_$1.tex
+    rm -f sermon_$1.mtc*
+    mv sermon_$1.pdf ../../
+    cd ..
+elif [ "$1" == "DSCCC" ] ; then
+    cd $1
+    xelatex sermon_$1_2009-present.tex
+    xelatex sermon_$1_2009-present.tex
+    rm -f sermon_$1_2009-present.mtc*
+    mv sermon_$1_2009-present.pdf ../../
+    cd ..
+elif [ "$1" == "HKBC" ] ; then
+    cd $1
+    xelatex sermon_$1_1928-2007.tex
+    xelatex sermon_$1_1928-2007.tex
+    rm -f sermon_$1_1928-2007.mtc*
+    mv sermon_$1_1928-2007.pdf ../../
+    cd ..
+    cd $1
+    xelatex sermon_$1_2008-present.tex
+    xelatex sermon_$1_2008-present.tex
+    rm -f sermon_$1_2008-present.mtc*
+    mv sermon_$1_2008-present.pdf ../../
+    cd ..
 elif [ "$1" == "JNG" ] ; then
     cd $1
     xelatex sermon_$1_2012-18.tex
@@ -52,25 +91,12 @@ elif [ "$1" == "JNG" ] ; then
     rm -f sermon_$1_2023-24.mtc*
     mv sermon_$1_2023-24.pdf ../../
     cd ..
-elif [ "$1" == "HKBC" ] ; then
+elif [ "$1" == "WWBS" ] ; then
     cd $1
-    xelatex sermon_$1_1928-2007.tex
-    xelatex sermon_$1_1928-2007.tex
-    rm -f sermon_$1_1928-2007.mtc*
-    mv sermon_$1_1928-2007.pdf ../../
-    cd ..
-    cd $1
-    xelatex sermon_$1_2008-present.tex
-    xelatex sermon_$1_2008-present.tex
-    rm -f sermon_$1_2008-present.mtc*
-    mv sermon_$1_2008-present.pdf ../../
-    cd ..
-elif [ "$1" == "DSCCC" ] ; then
-    cd $1
-    xelatex sermon_$1_2009-present.tex
-    xelatex sermon_$1_2009-present.tex
-    rm -f sermon_$1_2009-present.mtc*
-    mv sermon_$1_2009-present.pdf ../../
+    xelatex sermon_$1.tex
+    xelatex sermon_$1.tex
+    rm -f sermon_$1.mtc*
+    mv sermon_$1.pdf ../../
     cd ..
 elif [ "$1" == "YFCX" ] ; then
     cd $1
