77924811b39704f3eaca222c01f3d20caab80467
michael <michaelchan_wahyan@yahoo.com.hk>
2023-12-18 21:51:41 +0800

    [update] add DSCCC and YFCX to gen_index ci

--- .ci/1.4_gen_index.sh
+++ .ci/1.4_gen_index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +x
 pushd ../projects
-  for PROJECT_NAME in ACSMHK CBI CGST FVC JNG WWBS
+  for PROJECT_NAME in ACSMHK CBI CGST DSCCC FVC HKBC JNG WWBS YFCX
   do
     pushd ./$PROJECT_NAME
       python3 generate_index.py
