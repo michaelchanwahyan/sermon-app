4867165266ed70f78e3ab4fe52477763ea82f67a
pikachu-mini <>
2024-03-26 23:52:31 +0800

    [newfile] add clean i ij ijk script

---
+++ .ci/cleanse_repo_i_ij_ijk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set +x
+cd ..
+find . -name "i" -exec rm -f {} \;
+find . -name "i1" -exec rm -f {} \;
+find . -name "i2" -exec rm -f {} \;
+find . -name "i3" -exec rm -f {} \;
+find . -name "i4" -exec rm -f {} \;
+find . -name "j" -exec rm -f {} \;
+find . -name "j1" -exec rm -f {} \;
+find . -name "j2" -exec rm -f {} \;
+find . -name "j3" -exec rm -f {} \;
+find . -name "j4" -exec rm -f {} \;
+find . -name "ij" -exec rm -f {} \;
+find . -name "ijk" -exec rm -f {} \;
