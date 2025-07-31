4ab07c32b8868030893910612e6baae466c58db8
michael <michaelchan_wahyan@yahoo.com.hk>
2025-07-31 23:28:57 +0800

    [revamp] KFC sermon gen obtain dictionary from index

--- projects/KFC/generate_sermonbook.py
+++ projects/KFC/generate_sermonbook.py
@@ -16,17 +16,227 @@ import os.path
 import pickle as pkl
 import re
 #from re import compile as recompile
+import pandas as pd
 print('done !')
 
 
-with open("code_dictionary.pkl", "rb") as fp:
-    c2p_dict, bk2bkorder_dict, c2b_dict, c2ch_dict, c2v_dict, c2s_dict, c2t_dict, c2bvc_dict = pkl.load(fp)
-fp.close()
+bible_srcpath = '../../data/bible_src/cuv2/'
 
 
-with open("x2code_dictionary.pkl", "rb") as fp:
-    p2c_dict, b2c_dict = pkl.load(fp)
-fp.close()
+book_list_engsymbol = ['',
+    'Gen','Exo','Lev','Num','Deu',
+    'Jos','Jug','1Sa','2Sa','1Ki','2Ki',
+    'Isa','Jer','Eze',
+    'Hos','Joe','Amo','Oba','Jon','Mic',
+    'Nah','Hab','Zep','Hag','Zec','Mal',
+    'Psa','Pro','Job','Son','Rut','Lam','Ecc',
+    'Est','Dan','Ezr','Neh','1Ch','2Ch',
+    'Mat','Mak','Luk','Jhn','Act',
+    'Rom','1Co','2Co','Gal','Eph',
+    'Phl','Col','1Ts','2Ts',
+    '1Ti','2Ti','Tit','Phm','Heb',
+    'Jas','1Pe','2Pe','1Jn','2Jn',
+    '3Jn','Jud','Rev']
+
+
+# the bible books
+book_list = [
+    '創世記','出埃及記','利未記','民數記','申命記',
+    '約書亞記','士師記','撒母耳記上','撒母耳記下','列王記上','列王記下',
+    '以賽亞書','耶利米書','以西結書',
+    '何西阿書','約珥書','阿摩司書','俄巴底亞書','約拿書','彌迦書',
+    '那鴻書','哈巴谷書','西番雅書','哈該書','撒迦利亞書','瑪拉基書',
+    '詩篇','箴言','約伯記','雅歌','路得記','耶利米哀歌','傳道書',
+    '以斯帖記','但以理書','以斯拉記','尼希米記','歷代志上','歷代志下',
+    '馬太福音','馬可福音','路加福音','約翰福音','使徒行傳',
+    '羅馬書','哥林多前書','哥林多後書','加拉太書','以弗所書',
+    '腓立比書','腓利比書','歌羅西書','帖撒羅尼迦前書','帖撒羅尼迦後書',
+    '提摩太前書','提摩太後書','提多書','腓利門書','希伯來書',
+    '雅各書','彼得前書','彼得後書','約翰壹書','約翰一書','約翰貳書','約翰二書',
+    '約翰參書','約翰三書','猶大書','啟示錄']
+
+
+# the bible book ordering
+bk2bkorder_dict = {
+    '創世記':1, '出埃及記':2,'利未記':3,'民數記':4,'申命記':5,
+    '約書亞記':6,'士師記':7,
+    '撒母耳記上':8,'撒母耳記下':9,
+    '列王記上':10,'列王記下':11,
+    '以賽亞書':12,'耶利米書':13,'以西結書':14,
+    '何西阿書':15,'約珥書':16,'阿摩司書':17,'俄巴底亞書':18,'約拿書':19,'彌迦書':20,
+    '那鴻書':21,'哈巴谷書':22,'西番雅書':23,'哈該書':24,'撒迦利亞書':25,'瑪拉基書':26,
+    '詩篇':27,'箴言':28,'約伯記':29,'雅歌':30,'路得記':31,'耶利米哀歌':32,'傳道書':33,
+    '以斯帖記':34,'但以理書':35,'以斯拉記':36,'尼希米記':37,'歷代志上':38,'歷代志下':39,
+    '馬太福音':40,'馬可福音':41,'路加福音':42,'約翰福音':43,'使徒行傳':44,
+    '羅馬書':45,'哥林多前書':46,'哥林多後書':47,'加拉太書':48,'以弗所書':49,
+    '腓立比書':50,'腓利比書':50,'歌羅西書':51,'帖撒羅尼迦前書':52,'帖撒羅尼迦後書':53,
+    '提摩太前書':54,'提摩太後書':55,'提多書':56,'腓利門書':57,'希伯來書':58,
+    '雅各書':59,'彼得前書':60,'彼得後書':61,'約翰壹書':62,'約翰一書':62,'約翰貳書':63,'約翰二書':63,
+    '約翰參書':64,'約翰三書':64,'猶大書':65,'啟示錄':66
+}
+
+
+bkorder2bk_dict = {
+    0:'',
+    1:book_list[ 0], 2:book_list[ 1], 3:book_list[ 2], 4:book_list[ 3], 5:book_list[ 4],
+    6:book_list[ 5], 7:book_list[ 6], 8:book_list[ 7], 9:book_list[ 8],10:book_list[ 9],
+   11:book_list[10],12:book_list[11],13:book_list[12],14:book_list[13],15:book_list[14],
+   16:book_list[15],17:book_list[16],18:book_list[17],19:book_list[18],20:book_list[19],
+   21:book_list[20],22:book_list[21],23:book_list[22],24:book_list[23],25:book_list[24],
+   26:book_list[25],27:book_list[26],28:book_list[27],29:book_list[28],30:book_list[29],
+   31:book_list[30],32:book_list[31],33:book_list[32],34:book_list[33],35:book_list[34],
+   36:book_list[35],37:book_list[36],38:book_list[37],39:book_list[38],40:book_list[39],
+   41:book_list[40],42:book_list[41],43:book_list[42],44:book_list[43],45:book_list[44],
+   46:book_list[45],47:book_list[46],48:book_list[47],49:book_list[48],50:book_list[49],
+   51:book_list[50],52:book_list[51],53:book_list[52],54:book_list[53],55:book_list[54],
+   56:book_list[55],57:book_list[56],58:book_list[57],59:book_list[58],60:book_list[59],
+   61:book_list[60],62:book_list[61],63:book_list[62],64:book_list[63],65:book_list[64],
+   66:book_list[65],
+   67:''
+}
+
+
+c2p_dict   = {}
+c2b_dict   = {}
+c2ch_dict  = {}
+c2v_dict   = {}
+c2s_dict   = {}
+c2t_dict   = {}
+c2bvc_dict = {}
+
+df = pd.read_csv('index_byt.csv')
+for d in df.iterrows():
+    df_row = d[1]
+    c2p_dict[df_row['code']]  = df_row['preacher'] if not pd.isna(df_row['preacher']) else ' '
+    c2b_dict[df_row['code']]  = df_row['book']     if not pd.isna(df_row['book']) else ' '
+    c2ch_dict[df_row['code']] = df_row['chapter']  if not pd.isna(df_row['chapter']) else ' '
+    c2v_dict[df_row['code']]  = df_row['verse']    if not pd.isna(df_row['verse']) else ' '
+    c2s_dict[df_row['code']]  = df_row['title']
+    c2t_dict[df_row['code']]  = df_row['date']
+    c = df_row['code']
+    bvc = ''
+    bk = c2b_dict.get(c)
+    if bk is not None:
+        # ch = c2ch_dict.get(c)
+        v = c2v_dict.get(c)
+        if pd.isna(v):
+            continue
+        if v is not None and ':' in v:
+            if not bk in bk2bkorder_dict.keys():
+                # print(f'{bk} not in bk-2-bkorder dict keys !, skip and continue !')
+                continue
+            # print(bk, v)
+            srcfname = bible_srcpath + book_list_engsymbol[bk2bkorder_dict.get(bk)] + '.txt'
+            with open(srcfname, 'r') as fp:
+                bktxtlines = fp.readlines()
+            fp.close()
+            # --------------------------------------------------
+            # compile the starting verse A:B and
+            # ending verse C:D from v,
+            # then check coverage
+            # --------------------------------------------------
+            # cn := chapter number
+            # vn := verse number
+            # case 0 -> A:B
+            if v.count(':') == 1 and v.count('-') == 0:
+                vs = v.split(':')
+                cn1 = vs[0]
+                cn2 = cn1
+                vn1 = vs[1]
+                vn2 = vn1
+            # case 1 -> A:B-C
+            elif v.count(':') == 1 and v.count('-') == 1:
+                vs = v.split(':')
+                cn1 = vs[0]
+                cn2 = cn1
+                vn1 = vs[1].split('-')[0]
+                vn2 = vs[1].split('-')[1]
+            # case 2 -> A:B-C-D-...-X
+            elif v.count(':') == 1 and v.count('-') > 1:
+                vs = v.split(':')
+                cn1 = vs[0]
+                cn2 = cn1
+                #vn1 = vs[1].split('-')[0
+                #vn2 = vs[1].split('-')[-1]
+                vn1 = str(min([ int(_) for _ in vs[1].split('-') ]))
+                vn2 = str(max([ int(_) for _ in vs[1].split('-') ]))
+            # case 3 -> A:B-C:D where C > A
+            elif v.count(':') == 2 and v.count('-') == 1:
+                vs = v.split('-')
+                cn1 = vs[0].split(':')[0]
+                vn1 = vs[0].split(':')[1]
+                cn2 = vs[1].split(':')[0]
+                vn2 = vs[1].split(':')[1]
+            # case 4 -> otherwise, we dont handle it
+            else:
+                continue
+            # print concluded coverage
+            # print(f'{cn1}:{vn1} - {cn2}:{vn2}')
+            # --------------------------------------------------
+            # END OF check coverage
+            # --------------------------------------------------
+            header1 = cn1 + '.' + vn1
+            header2 = cn2 + '.' + vn2
+            # -- -- -- -- -- --
+            # cn and vn overflow handling:
+            # e.g. 12:1-18 but scripture actually only numbers
+            #      the verses 12:1-17, while 12:18 is just the splitted version of 12:17
+            # -- -- -- -- -- --
+            header2_exists = False
+            for bktxtline in bktxtlines:
+                if header2 == bktxtline[:len(header2)]:
+                    header2_exists = True
+                    break
+            # if overflow of verses number occurs,
+            # update ending verse to become the last verse
+            # of the same chapter of starting verse
+            h1_reached = False
+            header2_curr = header1
+            if not header2_exists:
+                ch_desired = c2ch_dict.get(c)
+                for bktxtline in bktxtlines:
+                    if not h1_reached:
+                        h1_reached = header1 in bktxtline[:len(header1)]
+                    if h1_reached:
+                        header2_prev = header2_curr
+                        header2_curr = bktxtline.split(' ')[0]
+                        ch_curr = bktxtline.split('.')[0]
+                        if ch_curr != ch_desired:
+                            header2 = header2_prev
+                            break
+                        else:
+                            header2 = header2_curr
+            # -- -- -- -- -- --
+            # END OF cn and vn overflow handling
+            # -- -- -- -- -- --
+            # loop through each line until reaching the starting verse
+            h1_reached = False
+            h2_reached = False
+            for bktxtline in bktxtlines:
+                # loop through each line until reaching the destinating chapter
+                if not h1_reached:
+                    h1_reached = header1 in bktxtline[:len(header1)]
+                if h1_reached:
+                    bvc += bktxtline
+                if not h2_reached:
+                    h2_reached = header2 in bktxtline[:len(header2)]
+                if h2_reached:
+                    break
+            bvc = bk + ' ' + v + '\n\n' + bvc
+            c2bvc_dict[c] = bvc
+
+p_list = sorted([ _ for _ in list(set(list(df['preacher']))) if not pd.isna(_) ])       
+print(sorted(p_list))
+
+
+# with open("code_dictionary.pkl", "rb") as fp:
+#     c2p_dict, bk2bkorder_dict, c2b_dict, c2ch_dict, c2v_dict, c2s_dict, c2t_dict, c2bvc_dict = pkl.load(fp)
+# fp.close()
+
+
+# with open("x2code_dictionary.pkl", "rb") as fp:
+#     p2c_dict, b2c_dict = pkl.load(fp)
+# fp.close()
 
 
 
