cbcfeecb2cd3fa1035271981449299be8e3f039b
pikachu <pikachu@pikachus-MacBook-Pro.local>
2026-07-21 00:16:32 +0800

    [update] ls rejection fsdate

--- projects/CHURCHK/churchk_download_by_code.py
+++ projects/CHURCHK/churchk_download_by_code.py
@@ -2,16 +2,18 @@ import os
 import sys
 import math
 
-if sys.argc > 1:
+if len(sys.argv) > 1:
     code_ = sys.argv[1]
 else:
     exit()
 
 code_ = int(code_)
-code_parent_ = math.floor(code_ / 1000)
+code_str = "%06d" % code_
+code_parent_ = math.floor(code_ / 1000) * 1000
+code_parent_str = str(code_parent_)
 
-cmd_str = 'wget -O ./church.com.hk.html/' + str(code_) + '.html"https://www.church.com.hk/acms/content.asp?site=cdc&op=show&type=product&code=' + str(code_) + '&layout=sermon"'
+cmd_str = 'wget -O ./church.com.hk.html/' + code_str + '.html "https://www.church.com.hk/acms/content.asp?site=cdc&op=show&type=product&code=' + code_str + '&layout=sermon"'
 _ = os.system(cmd_str)
-cmd_str = 'wget -O ' + str(code_) + '.mp3 https://www.church.com.hk/ftp/mp3/' + str(code_parent_) + '/' + str(code_) + '.mp3'
+cmd_str = 'wget -O ' + code_str + '.mp3 https://www.church.com.hk/ftp/mp3/' + code_parent_str + '/' + code_str + '.mp3'
 _ = os.system(cmd_str)
 
